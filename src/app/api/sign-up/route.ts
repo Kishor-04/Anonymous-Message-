a3925182e7dfdb9829f40206ad4045e354a81d5e
Kishor-04 <kishorpatil.connect@gmail.com>
2025-10-05 21:41:48 +0530

    Changes password to hashed password

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: Request) {
             const newUser = new UserModel({
                 username,
                 email,
-                password,
+                password :hashedPassword,
                 verifyCode,
                 verifyCodeExpiry: expiryDate,
                 isVerified: false,
